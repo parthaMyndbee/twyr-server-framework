9c26eb96bc5b58e09ac68dcdc86c154ae20b8561
Vish Desai <vish@myndbee.com>
2016-03-11 20:32:31 +0530

    Added the first-cut of a configuration service - no functionality yet

---
+++ app/modules/services/configuration-service/service.js
@@ -0,0 +1,36 @@
+/*
+ * Name			: app/modules/services/configuration-service/service.js
+ * Author		: Vish Desai (vishwakarma_d@hotmail.com)
+ * Version		: 0.9.1.2
+ * Copyright	: Copyright (c) 2014 - 2016 Vish Desai (https://www.linkedin.com/in/vishdesai).
+ * License		: The MITNFA License (https://spdx.org/licenses/MITNFA.html).
+ * Description	: The Twy'r Server Configuration Service
+ *
+ */
+
+"use strict";
+
+/**
+ * Module dependencies, required for ALL Twy'r modules
+ */
+var base = require('./../service-base').baseService,
+	prime = require('prime'),
+	promises = require('bluebird');
+
+/**
+ * Module dependencies, required for this module
+ */
+
+var configurationService = prime({
+	'inherits': base,
+
+	'constructor': function(module) {
+		base.call(this, module);
+	},
+
+	'name': 'configuration-service',
+	'dependencies': ['database-service']
+});
+
+exports.service = configurationService;
+
